fb6b75627de39b879f0a7a2a5f5b8f4ea0cf8e9c
wonnieIT <138439779+wonnieIT@users.noreply.github.com>
2023-07-04 14:59:22 +0900

    Update umamusume.py

--- games/umamusume.py
+++ games/umamusume.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
