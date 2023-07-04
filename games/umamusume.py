cebbd39cdf749a0cefc47ad37225391c72c7fcba
wonnieIT <138439779+wonnieIT@users.noreply.github.com>
2023-07-04 14:45:43 +0900

    Update umamusume.py

--- games/umamusume.py
+++ games/umamusume.py
@@ -25,7 +25,7 @@ CAFE_NAME = 'umamusume-kor'
 
 REQ_BOARD_NAME = 'ZaXF' 
 
-driver = webdriver.Chrome('/home/appuser/.cache/selenium/chromedriver/linux64/114.0.5735.90/chromedriver')
+driver = webdriver.Chrome('./../chromedriver')
 
 page = driver.get(f'https://cafe.daum.net/umamusume-kor/{REQ_BOARD_NAME}')
 
